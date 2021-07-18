7d5d6c851d8e2ca88c894c1c8367e0d209e6c865
samueljuma <samueljuma1024@gmail.com>
2021-07-18 10:48:41 +0300

    font-awsome icons in use

---
+++ my-app/src/Components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+
+function Navigation(){
+    return(
+        <nav>
+            <FontAwesomeIcon
+                icon = {faBars}
+            />
+        </nav>
+    )
+}
+
+export default Navigation
