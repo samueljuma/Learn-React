4fad858b5f8705fc906ad1c4c6ec287a23fac7f7
samueljuma <samueljuma1024@gmail.com>
2021-07-18 21:14:27 +0300

    add react-router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,11 @@
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
