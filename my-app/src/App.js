5112dd6458dfff8208c43067520908b16fe8497d
samueljuma <samueljuma1024@gmail.com>
2021-07-18 05:53:38 +0300

    my-app:initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HelloWorld from './Components/HelloWorld';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
